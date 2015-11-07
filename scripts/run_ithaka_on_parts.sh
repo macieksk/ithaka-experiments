c7716e763d9edd76159b3e1fc0c4367248fa46fe
Maciek Sykulski <macieksk@gmail.com>
2015-11-07 07:39:24 +0100

    script run parts

--- scripts/run_ithaka_on_parts.sh
+++ scripts/run_ithaka_on_parts.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+LOG="run_parts_script.log"
+OUTD="query_results/"
+
 for f in reads/all_parts/1/*/stdout.fa.gz; 
 do 
 	NUM="`echo $f | cut -d '/' -f 4`"
-	echo Processing part number $NUM | tee $LOG
-	time ithaka-query.py -i db/workdir/index_h16_s#########################/ -r query_results/ -m 16G -t 8 $f 2>&1 > $LOG
-	mv query_results/results_stdout.fa.gz.bam query_results/results_stdout_$NUM.fa.gz.bam
-	time samtools sort -@ 8 -n results_stdout_$NUM.fa.gz.bam results_stdout_$NUM.fa.gz_readsorted 2>&1 > $LOG 
+	echo Processing part number $NUM | tee -a $LOG
+	{ time ithaka-query.py -i db/workdir/index_h16_s#########################/ -r $OUTD -m 16G -t 8 $f ; } >> $LOG 2>&1
+	mv $OUTD/results_stdout.fa.gz.bam $OUTD/results_stdout_$NUM.fa.gz.bam
+	{ time samtools sort -@ 8 -n $OUTD/results_stdout_$NUM.fa.gz.bam $OUTD/results_stdout_$NUM.fa.gz_readsorted ; } >> $LOG 2>&1  
 done 
 
