7a076a8f87945ec854919be6c06d833312b4dcf4
Maciek Sykulski <macieksk@gmail.com>
2015-11-08 14:56:28 +0100

    scripts

--- reads/split_into_parts2.sh
+++ reads/split_into_parts2.sh
@@ -1,4 +1,4 @@
 zcat all_reads_with_names.fa.gz | parallel --pipe --recstart '>' --block 512M --files --results all_parts  gzip -1 ::: `seq -w 1 99`
 
+rename stdout stdout.fa.gz all_parts/1/*/stdout
 
-#rm all_parts -r
