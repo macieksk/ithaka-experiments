7ab9c1e8f05296300a6a0e1e73ce62a2005836c9
Maciek Sykulski <macieksk@gmail.com>
2015-11-10 02:17:56 +0100

    jellyfish scripts

--- scripts/jellyfish_exact.sh
+++ scripts/jellyfish_exact.sh
@@ -1,2 +1,2 @@
-jellyfish dump db/workdir/blooms_unionall/UNION_ALL_s#########################.jellyfish -L $1 -U $1 | wc -l | sed "s/$/\/2/" | bc | sed "s/$/ "$1"/"
+jellyfish dump db/workdir/blooms_unionall/UNION_ALL_s$2.jellyfish -L $1 -U $1 | wc -l | sed "s/$/\/2/" | bc | sed "s/$/ "$1" '"$2"'/"
 #echo -e 'a\nb' | wc -l | sed "s/$/\/2/" | bc | sed "s/$/ "$1"/" 
--- scripts/stats_from_jellyfish.sh
+++ scripts/stats_from_jellyfish.sh
@@ -1,3 +1,3 @@
-OUT="stats_from_jellyfish.csv"
-parallel  -k scripts/jellyfish_exact.sh {} ::: `seq 151 698` >> $OUT
+OUT="stats_from_jellyfish_many_seeds.csv"
+parallel -k scripts/jellyfish_exact.sh {} {} ::: `seq 2 700` ::: `cat seeds_to_jellyfish.txt` >> $OUT
 
